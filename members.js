37163a9839672736da0d33a21c3770da19f9c75d
jariverism <jariverism@gmail.com>
2026-07-25 23:31:26 +0900

    Add member data module for v3

---
+++ members.js
@@ -0,0 +1,15 @@
+(function(global){
+'use strict';
+global.HinataMembers=[
+ {id:'ig',name:'井口眞緒',type:'トーク型',stats:[17,31,27,57,36],special:'talk'},
+ {id:'ush',name:'潮紗理菜',type:'安定型',stats:[18,40,39,42,43],special:'support'},
+ {id:'mem',name:'柿崎芽実',type:'表現型',stats:[20,43,45,38,58],special:'expression'},
+ {id:'kat',name:'加藤史帆',type:'万能型',stats:[21,49,48,46,55],special:'popular'},
+ {id:'kyo',name:'齊藤京子',type:'歌唱型',stats:[23,64,37,44,52],special:'sing'},
+ {id:'kum',name:'佐々木久美',type:'統率型',stats:[18,39,43,52,44],special:'staff'},
+ {id:'mir',name:'佐々木美玲',type:'ライブ型',stats:[20,54,53,46,49],special:'live'},
+ {id:'tak',name:'高瀬愛奈',type:'知性型',stats:[16,40,39,44,42],special:'exam'},
+ {id:'aya',name:'高本彩花',type:'モデル型',stats:[19,38,46,41,59],special:'model'},
+ {id:'mei',name:'東村芽依',type:'身体型',stats:[18,37,61,31,51],special:'dance'}
+];
+})(window);
